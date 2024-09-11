0c7a7da1a3f02891815c07eeb5f9d35677bd1487
Elina Belousova <113827202+BelElina@users.noreply.github.com>
2024-09-11 21:02:30 +0400

    Update Main.java

--- src/main/java/com/elksy/Main.java
+++ src/main/java/com/elksy/Main.java
@@ -2,6 +2,6 @@ package com.elksy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, qa_guru!");
+        System.out.println("Hello, qa_guru!!! :)");
     }
 }
